5c32793f747972d83c5aeac4f5d852bcdc4300ac
Mike <hackclarkak32@gmail.com>
2020-12-05 16:01:13 +0300

    v.004:Fixed Counter, main file

--- classes.py
+++ classes.py
@@ -83,15 +83,18 @@ class TokenCreator:
 
 		"""
 		tokens = []
-		x = 0
-		for char in self.expression:
-			if char.isdigit():
-				x = x*10 + float(char)
-			else:
-				if x != 0:
-					tokens.append(x)
+		fp, lp = (None, None)
+
+		for i,char in enumerate(self.expression):
+			if char.isdigit() and fp is None:
+				fp = i
+			elif not char.isdigit() and char != '.':
+				lp = i
+				if self.expression[fp:lp]:
+					tokens.append(float(self.expression[fp:lp]))
 				tokens.append(char)
-				x = 0
+				fp, lp = (lp+1, None)
+
 		return self._replace_all_minsum_wnegative(tokens[:len(tokens)-1])
 
 	def _count_neg_nums(self, tokens):
@@ -116,6 +119,72 @@ class TokenCreator:
 		self.expression = expression + ' '
 		self.ExpressionValidator = ExpressionValidator(expression)
 
+class Counter:
+	def __init__(self, expression):
+		self.expression = expression
+		self.tokens = TokenCreator(expression).get_tokens()
+
+	def _find(self,tokens, match):
+		for index, element in enumerate(tokens):
+			if match == element:
+				return index
+
+	def _rfind(self,tokens, match):
+		for index, element in enumerate(tokens[::-1]):
+			if match == element:
+				return len(tokens)-index-1
+
+	def _count_nonbracket_exp(self, tokens): 
+		for operator, function in functions.binary_operators.items():
+			if self._find(tokens, operator) is not None:
+				left = tokens[:self._find(tokens, operator)]
+				right = tokens[self._find(tokens, operator)+1:]
+
+				if len(left)==1 and len(right)==1:
+					return function(
+						left[0],
+						right[0]
+					)
+				elif len(left)==1 and type(left[0]) is float :
+					return function(
+						left[0],
+						self._count_nonbracket_exp(right)
+					)
+				elif len(right)==1 and type(right[0]) is float:
+					return function(
+						self._count_nonbracket_exp(left),
+						right[0]
+					)
+				else:
+					return function(
+						self._count_nonbracket_exp(left),
+						self._count_nonbracket_exp(right),
+					)
+	def _count_exp(self, tokens):
+		if tokens.count('(') != 0:
+			for _ in range(tokens.count('(')):
+				deepest_opened_bracket = self._rfind(tokens, '(')
+				deepest_closed_bracket = self._find(tokens[deepest_opened_bracket:], ')')
+
+				before = tokens[:deepest_opened_bracket]
+				bracketed_exp_result = self._count_nonbracket_exp(tokens[deepest_opened_bracket+1:deepest_opened_bracket+deepest_closed_bracket])
+				after = tokens[deepest_opened_bracket+deepest_closed_bracket+1:]
+
+				tokens = tokens[:deepest_opened_bracket]
+				tokens.append(bracketed_exp_result)
+
+				for element in after:
+					tokens.append(element)
+
+				return self._count_nonbracket_exp(tokens)
+		else:
+			return self._count_nonbracket_exp(tokens)
+
+
+
+	def get_result(self):
+		tokens = self.tokens
+		return self._count_exp(tokens)
 
 # class ExpressionModifier:
 # 	def __init__(self, expression):
--- main.py
+++ main.py
@@ -1,73 +1,30 @@
-from classes import TokenCreator
-import functions
+from classes import Counter
+from exceptions import NotPairedBracketError, LettersFoundError
 
-class Counter:
-	def __init__(self, expression):
-		self.expression = expression
-		self.tokens = TokenCreator(expression).get_tokens()
+if __name__ == '__main__':
 
-	def _find(self,tokens, match):
-		for index, element in enumerate(tokens):
-			if match == element:
-				return index
+	my_input = str(input("Введите ваше выражение:"))
+	result = None
+	while my_input != 'qqq':
+		try:
+			result = Counter(my_input).get_result()
 
-	def _rfind(self,tokens, match):
-		for index, element in enumerate(tokens[::-1]):
-			if match == element:
-				return len(tokens)-index-1
+			if result is None:
+				print('Чето ты не то делаешь....')
+			else: print(result)
+			my_input = str(input("Введите ваше выражение:"))
 
-	def _count_nonbracket_exp(self, tokens): 
-		for operator, function in functions.binary_operators.items():
-			if self._find(tokens, operator) is not None:
-				left = tokens[:self._find(tokens, operator)]
-				right = tokens[self._find(tokens, operator)+1:]
+		except NotPairedBracketError:
+			print('Найдено разное количество открывающихся и закрывающихся скобок.')
+			my_input = str(input("Введите ваше выражение:"))
+			continue
 
-				if len(left)==1 and len(right)==1:
-					return function(
-						left[0],
-						right[0]
-					)
-				elif len(left)==1 and type(left[0]) is float :
-					return function(
-						left[0],
-						self._count_nonbracket_exp(right)
-					)
-				elif len(right)==1 and type(right[0]) is float:
-					return function(
-						self._count_nonbracket_exp(left),
-						right[0]
-					)
-				else:
-					return function(
-						self._count_nonbracket_exp(left),
-						self._count_nonbracket_exp(right),
-					)
-	def _count_exp(self, tokens):
-		if tokens.count('(') != 0:
-			for _ in range(tokens.count('(')):
-				deepest_opened_bracket = self._rfind(tokens, '(')
-				deepest_closed_bracket = self._find(tokens[deepest_opened_bracket:], ')')
+		except LettersFoundError:
+			print('Найдены буквы.')
+			my_input = str(input("Введите ваше выражение:"))
+			continue
 
-				before = tokens[:deepest_opened_bracket]
-				bracketed_exp_result = self._count_nonbracket_exp(tokens[deepest_opened_bracket+1:deepest_opened_bracket+deepest_closed_bracket])
-				after = tokens[deepest_opened_bracket+deepest_closed_bracket+1:]
-
-				tokens = tokens[:deepest_opened_bracket]
-				tokens.append(bracketed_exp_result)
-
-				for element in after:
-					tokens.append(element)
-
-				return self._count_nonbracket_exp(tokens)
-		else:
-			return self._count_nonbracket_exp(tokens)
-
-
-
-	def get_result(self):
-		tokens = self.tokens
-		print(self.tokens)
-		return self._count_exp(tokens)
-		
-a = Counter('5+2*(3+2)').get_result()
-print(a)
+		except Exception as e:
+			print('Хз че случилось')
+			my_input = str(input("Введите ваше выражение:"))
+			continue
